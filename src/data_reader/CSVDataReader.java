f0063d1b34fd9d561d52ff35d44ad45aa57cdbe7
Vegh Adam <veghadam1991@gmail.com>
2016-03-16 17:24:37 +0100

    sorry, but I can't finished. I get shock...

--- src/data_reader/CSVDataReader.java
+++ src/data_reader/CSVDataReader.java
@@ -1,5 +1,10 @@
 package data_reader;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Set;
 
@@ -11,6 +16,31 @@ public class CSVDataReader extends DataReader{
 	
 	public CSVDataReader(String csvFilePath) {
 		this.csvFilePath = csvFilePath;
+		persons = new ArrayList<>();
+		
+		BufferedReader buffReader;
+		String line;
+		ArrayList<String> dataRowList = new ArrayList<>();
+		try {
+			buffReader = new BufferedReader(new FileReader(this.csvFilePath));
+			while ((line = buffReader.readLine()) != null) {
+				dataRowList.add(line);
+			}
+		}
+		catch (Exception e) {
+			System.out.println(e);
+		}
+		
+		String header = dataRowList.get(0);
+		List<String> listHeader = new ArrayList<>();
+		listHeader = Arrays.asList(header.split(","));
+		
+		HashMap<Integer, String> mapHeader = new HashMap<>();
+		for (Integer i = 0; i < listHeader.size(); i++) {
+			mapHeader.put(i, listHeader.get(i));
+		}
+		
+		
 	}
 	
 	@Override
@@ -27,5 +57,8 @@ public class CSVDataReader extends DataReader{
 	public Set<Person> getPersons() {
 		return null;
 	}
-
+	
+	public static void main(String[] args) {
+		new CSVDataReader("Documentation/persons.csv");
+	}
 }
---
+++ src/server/PersonStoreServerSocket.java
@@ -0,0 +1,9 @@
+package server;
+
+import data_reader.DataReader;
+
+public class PersonStoreServerSocket {
+	private DataReader store;
+	public void start() {
+	}
+}
